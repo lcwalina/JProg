7c97d72302d91f973b79e6e0ebc4a69e6cf67120
Lukasz Cwalina <lcwalina@sigma.ug.edu.pl>
2016-03-04 00:40:16 +0100

    zadanie3

---
+++ lab1/zad3.c
@@ -0,0 +1,17 @@
+//Napisać programy testujące działanie operatorów z efektami ubocznymi w różnych sytuacjach, od których może zależeć wynik działania.
+
+#include<stdio.h>
+int main()
+{
+  int a,b,c,tab[2];
+  a=b=c=0;
+  tab[0]=tab[1]=0;
+  tab[a++]=10;
+  printf("tab[0]=%d\ntab[1]=%d\n",tab[0], tab[1]);
+  a=0;
+  tab[0]=tab[1]=0;
+  tab[++a]=10;
+  printf("tab[0]=%d\ntab[1]=%d\n",tab[0], tab[1]);
+
+  return 0;
+}
