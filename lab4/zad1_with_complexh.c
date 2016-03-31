23ba7093b8e7e7af368ca515a60d725b74317562
Łukasz Cwalina <lcwalina@sigma.ug.edu.pl>
2016-03-31 15:43:38 +0200

    edit

--- lab4/zad1_with_complexh.c
+++ lab4/zad1_with_complexh.c
@@ -6,16 +6,16 @@ int main() {
     double complex z1 = 1.0 + 3.0 * I;
     double complex z2 = 1.0 - 4.0 * I;
 
-    double complex suma = z1 + z2;
-    double complex roznica = z1 - z2;
-    double complex iloczyn = z1 * z2;
+    double complex sum = z1 + z2;
+    double complex difference = z1 - z2;
+    double complex product = z1 * z2;
     double complex sprz_z1 = conj(z1);
     double complex sprz_z2 = conj(z2);
 
     printf( "z1 = %.2f%+.2fi\t\tz2 = %.2f%+.2fi\n", creal(z1), cimag(z1), creal(z2), cimag(z2));
-    printf("z1 + z2 = %.2f%+.2fi\n", creal(suma), cimag(suma));
-    printf("z1 - z2 = %.2f%+.2fi\n", creal(roznica), cimag(roznica));
-    printf("z1 * z2 = %.2f%+.2fi\n", creal(iloczyn), cimag(iloczyn));
+    printf("z1 + z2 = %.2f%+.2fi\n", creal(sum), cimag(sum));
+    printf("z1 - z2 = %.2f%+.2fi\n", creal(difference), cimag(difference));
+    printf("z1 * z2 = %.2f%+.2fi\n", creal(product), cimag(product));
     printf("z1 = %.2f%+.2fi\n", creal(sprz_z1), cimag(sprz_z1));
     printf("z2 = %.2f%+.2fi\n", creal(sprz_z2), cimag(sprz_z2));
 
